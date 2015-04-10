2f256389c4c107fcf44e010397e7ffecf116de02
Feng Pan <panfeng0007@gmail.com>
2015-04-10 01:09:54 -0400

    nothing big

--- List/List.hpp
+++ List/List.hpp
@@ -6,31 +6,51 @@
 
 namespace fl
 {
-	template <class T> 
-		struct Iterator
+	namespace containers
 	{
-		bool operator==(const Iterator& rhs);
+		
+		template <class T> struct Iterator
+		{
+			bool operator==(const Iterator& rhs);
 
-	};
+		};
 
-	template <class T> 
-		struct InputIterator
-	{
-	};
+		template <class T> struct InputIterator
+		{
+		};
 
-	template <class T>
-		struct ReverseIterator
-	{
-	};
-	template <class T> class List
-	{
-		using difference_type = std::ptrdiff_t;
-		using size_type = std::size_t;
-		using pointer_type = T*;
-		using reference = T&;
-		using const_reference = const T&;
+		template <class T> struct ReverseIterator
+		{
+		};
 
-		public:
+		template <class T> struct Node
+		{
+
+			Node() 
+			{
+				m_next = this;
+				m_prev = this;
+				m_value = nullptr;
+			}
+			~Node() {}
+			T* m_value;
+			Node* m_next;
+			Node* m_prev;
+		};
+
+		template <class T> class List
+		{
+			using difference_type = std::ptrdiff_t;
+			using node_ptr = Node<T>*;
+			using size_type = std::size_t;
+			using value_type = T;
+			using pointer_type = value_type*;
+			using reference = value_type&;
+			using const_reference = const value_type&;
+			using node_reference = Node<T>&;
+			using const_node_reference = const Node<T>&;
+
+			public:
 			List() {}
 			List( std::size_t count, const T& value);
 			explicit List( size_type count );
@@ -39,21 +59,15 @@ namespace fl
 			List(const List& other) : 
 				m_head(other.m_head),
 				m_tail(other.m_tail),
-				m_next(other.m_next),
-				m_prev(other.m_prev),
 				m_count(other.m_count) {}
 			List(List&& other) 
 			{
 				this->m_head = other.m_head;
 				this->m_tail = other.m_tail;
-				this->m_next = other.m_next;
-				this->m_prev = other.m_prev;
 				this->m_count = std::move(other.m_count);
 
 				other.m_head = nullptr;
 				other.m_tail = nullptr;
-				other.m_next = nullptr;
-				other.m_prev = nullptr;
 			}
 			List(std::initializer_list<T> init);
 			~List() 
@@ -75,17 +89,13 @@ namespace fl
 
 			}
 
-
-
-			
-		private:
-			pointer_type m_head;
-			pointer_type m_tail;
-			pointer_type m_next;
-			pointer_type m_prev;
+			private:
+			node_ptr m_head;
+			node_ptr m_tail;
 			size_type m_count;
-			
-	};
+
+		};
+	}
 
 }
 
