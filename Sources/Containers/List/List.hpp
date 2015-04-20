7e2451e1eced8d6fbdeecb9eb77f8b6c24f41031
Feng Pan <panfeng0007@gmail.com>
2015-04-19 20:49:44 -0400

    insert function almost done

--- Sources/Containers/List/List.hpp
+++ Sources/Containers/List/List.hpp
@@ -24,7 +24,7 @@ namespace fl
 			Node_base& operator=(Node_base& other) 
 			{
 				m_next = other.m_next;
-				m_prev = other.m_prev;	
+				m_prev = other.m_prev;
 
 				return *this;
 			}
@@ -63,92 +63,157 @@ namespace fl
 				using const_reference = const value_type&;
 
 			private:
-				template <class Tp, typename = void>
-				struct is_iterator
+				node_ptr m_acquire_node()
 				{
-					static constexpr bool value = false;
+					try
+					{
+						typename decltype(m_alloc)::template rebind<node_type>::other m_node_alloc;
+						return m_node_alloc.allocate(1);
+					}
+					catch (const std::bad_alloc& e)
+					{
+						std::cerr << "Allocation of node failed." << std::endl;
+					}
+				}
+
+				void m_construct_node(node_ptr node, reference value)
+				{
+					typename decltype(m_alloc)::template rebind<node_type>::other m_node_alloc;
+					m_node_alloc.construct(node, value);
+				}
+
+				void m_destroy_node(node_ptr node)
+				{
+					typename decltype(m_alloc)::template rebind<node_type>::other m_node_alloc;
+					m_node_alloc.destroy(node);
+				}
+
+				void m_put_node(node_ptr node)
+				{
+					typename decltype(m_alloc)::template rebind<node_type>::other dealloc;
+					dealloc.destroy(node);
+					dealloc.deallocate(node, 1);
+					node = nullptr;
+				}
+
+				void m_fill_initialize(size_type count,
+						const T& value,
+						const Allocator& alloc = Allocator())
+				{
+					m_count = count;
+					m_alloc = alloc;
+					m_sentinal = m_acquire_node();
+					m_construct_node(m_sentinal, 0);
+					node_ptr np;
+					for (int i = 0; i < (int) count; i++)
+					{
+						np = m_acquire_node();
+						m_construct_node(np, value);
+						np++;
+					}
+					np = np - count + 1;
+					for (int i = 0; i < (int) count-1; i++)
+					{
+						np[i].m_next = &np[i+1];
+						np[i+1].m_prev = &np[i];
+					}
+
+					m_head = np;
+					m_tail = np[count-1];
+
+					m_head->m_prev = m_sentinal;
+					m_tail->m_next = m_sentinal;
+				}
+
+				template <bool flag_is_const, class Const, class No_const> struct type_picker {};
+				template <class Const, class No_const> struct type_picker<true, Const, No_const>
+				{
+					using type = Const;
 				};
 
-				template <class Tp>
-				struct is_iterator<Tp, 
-								   typename std::enable_if<!std::is_same<typename fl::iterators::IteratorTraits<Tp>::value_type, void>::value>::type>
+				template <class Const, class No_const> struct type_picker<false, Const, No_const>
 				{
-					static constexpr bool value = true;
+					using type = No_const;
 				};
-				template <bool flag_is_const, class True, class False> struct type_picker {};
-				template <class True, class False> struct type_picker<true, True, False>
-				{
-					using type = True;
-				};
-
-				template <class True, class False> struct type_picker<false, True, False>
-				{
-					using type = False;
-				};
-				template <class U, bool is_const = false> class Iterator
+				template <class U, bool is_const = false> struct Iterator
 				{
 
-					public:
-						using iterator_category = fl::iterators::BidirectionalIteratorTag;
-						using value_type = typename type_picker<is_const, const U, U>::type;
-						using difference_type = std::ptrdiff_t;
-						using pointer = typename type_picker<is_const, const U*, U*>::type;
-						using reference = typename type_picker<is_const, const U&, U&>::type;
-						using node_ptr = typename type_picker<is_const, const ListNode<U>*, ListNode<U>*>::type;
-					
-					private:
-						node_ptr m_node;	
+					using iterator_category = fl::iterators::BidirectionalIteratorTag;
+					using value_type = typename type_picker<is_const, const U, U>::type;
+					using difference_type = std::ptrdiff_t;
+					using pointer = typename type_picker<is_const, const U*, U*>::type;
+					using reference = typename type_picker<is_const, const U&, U&>::type;
+					using node_ptr = typename type_picker<is_const, const ListNode<U>*, ListNode<U>*>::type;
 
-					public:
-						Iterator() {}
-						Iterator(node_ptr p) : m_node(p) {}
-						Iterator(const Iterator& other) : m_node(other.m_node) {}
-						~Iterator() {}
+					node_ptr m_node;	
 
-						reference operator*() const
-						{
-							return m_node->m_data;
-						}
-						pointer operator->() const
-						{
-							return std::addressof(operator*());
-						}
-						Iterator& operator++()
-						{
-							m_node = dynamic_cast<node_ptr>(m_node->m_next);
-							return *this;
-						}
-						Iterator operator++(int)
-						{
-							Iterator it(*this);
-							m_node = dynamic_cast<node_ptr>(m_node->m_next);
-							return it;
-						}
+					Iterator() {}
+					Iterator(node_ptr p) : m_node(p) {}
+					Iterator(const Iterator& other) : m_node(other.m_node) {}
+					~Iterator() {}
 
-						Iterator& operator--()
-						{
-							m_node = dynamic_cast<node_ptr>(m_node->m_prev);
-							return *this;
-						}
-						Iterator operator--(int)
-						{
-							Iterator it(*this);
-							m_node = dynamic_cast<node_ptr>(m_node->m_prev);
-							return it;
-						}
+					reference operator*() const
+					{
+						return m_node->m_data;
+					}
+					pointer operator->() const
+					{
+						return &(operator*());
+					}
+					Iterator& operator++()
+					{
+						m_node = dynamic_cast<node_ptr>(m_node->m_next);
+						return *this;
+					}
+					Iterator operator++(int)
+					{
+						Iterator it(*this);
+						m_node = dynamic_cast<node_ptr>(m_node->m_next);
+						return it;
+					}
 
-						bool operator==(const Iterator& rhs)
-						{
-							return this->m_node == rhs.m_node;
-						}
+					Iterator& operator--()
+					{
+						m_node = dynamic_cast<node_ptr>(m_node->m_prev);
+						return *this;
+					}
+					Iterator operator--(int)
+					{
+						Iterator it(*this);
+						m_node = dynamic_cast<node_ptr>(m_node->m_prev);
+						return it;
+					}
 
-						bool operator!=(const Iterator& rhs)
+					Iterator operator-(int n) const
+					{
+						node_ptr tmp = this->m_node;
+						for (int i = 0 ; i < n; i++)
 						{
-							return this->m_node != rhs.m_node;
+							tmp = tmp->m_prev;
 						}
+						return Iterator(tmp);
+					}
 
-						
-					
+					Iterator operator+(int n) const
+					{
+						node_ptr tmp = this->m_node;
+						for (int i = 0 ; i < n; i++)
+						{
+							tmp = tmp->m_next;
+						}
+						return Iterator(tmp);
+					}
+
+					bool operator==(const Iterator& rhs)
+					{
+						return this->m_node == rhs.m_node;
+					}
+
+					bool operator!=(const Iterator& rhs)
+					{
+						return this->m_node != rhs.m_node;
+					}
+
 				};	
 			public:
 				using iterator = Iterator<T>;
@@ -159,100 +224,68 @@ namespace fl
 				explicit List(const Allocator& alloc = Allocator()) 
 				{ 
 					m_alloc = alloc;
-					typename decltype(m_alloc)::template rebind<node_type>::other m_node_alloc;
-					node_base_ptr np = m_node_alloc.allocate(1);
-					m_node_alloc.construct(np, 0);
-					m_sentinal = np;
+					m_count = 0;
+					m_sentinal = m_acquire_node();
+					m_construct_node(m_sentinal, 0);
 					m_head = m_sentinal;
 					m_tail = m_sentinal;
-					m_count = 0;
 				}
 
 				List( size_type count, 
 						const T& value, 
 						const Allocator& alloc = Allocator() )
 				{
-					m_count = count;
-					m_alloc = alloc;
-					typename decltype(m_alloc)::template rebind<node_type>::other m_node_alloc;
-
-					try
-					{
-						node_ptr np = m_node_alloc.allocate(count);
-						size_type i = 0;
-						while (i < count)
-						{
-							m_node_alloc.construct(np+i, value);
-							i++;
-						}
-
-						m_sentinal = m_node_alloc.allocate(1);
-						m_node_alloc.construct(m_sentinal, 0);
-						m_head = &np[0];
-						m_head->m_prev = m_sentinal;
-						m_sentinal->m_next = m_head;
-						i = 1;
-						node_ptr temp = m_head;
-						while (i < count)
-						{
-							temp->m_next = &np[i];
-							temp->m_next->m_prev = temp;
-							temp = (node_ptr) temp->m_next;
-							i++;
-						}
-						m_tail = temp;
-						m_tail->m_next = m_sentinal;
-						m_sentinal->m_prev = m_tail;
-						temp = nullptr;
-					}
-					catch (const std::bad_alloc& ex)
-					{
-						std::cout << "allocation failed." << std::endl;
-					}
-
+					m_fill_initialize(count, value, alloc);
 				}
 
-				// C++14
-				explicit List( size_type count,
-						const Allocator& alloc = Allocator())
+				List(List&& other)
 				{
-					pointer p = alloc.allocate(count);
-					m_count = count;
-					m_alloc = alloc;
+					m_head = other.m_head;
+					m_tail = other.m_tail;
+					m_sentinal = other.m_sentinal;
+
+					other.m_head = nullptr;
+					other.m_tail = nullptr;
+					other.m_sentinal = nullptr;
+
+					m_count = std::move(other.m_count);
+				}
+				List(List&& other, const Allocator& alloc)
+				{
+					m_head = other.m_head;
+					m_tail = other.m_tail;
+					m_sentinal = other.m_sentinal;
+
+					other.m_head = nullptr;
+					other.m_tail = nullptr;
+					other.m_sentinal = nullptr;
+
+					m_count = std::move(other.m_count);
+					m_alloc = std::move(alloc);
 				}
 
-				List(List&& other);
-				List(List&& other, const Allocator& alloc);
-				/*
 				template <class InputIterator>
 				List(InputIterator first, 
 					 InputIterator last,
 					 const Allocator& alloc = Allocator())
 				{
-					if (is_iterator<InputIterator>::value)
+					if (std::is_integral<InputIterator>::value)
 					{
-
-
+						m_fill_initialize(first, last, alloc);
 					}
 					else
 					{
-						
-						List(first, last, alloc);
 					}
 				}
-				*/
+
 				List(std::initializer_list<T> init)
 				{
 					m_count = init.size();
-					for (auto it = init.begin(); it != init.end(); ++it)
-					{
-
-					}
-
 				}
 				~List() 
 				{
-					
+					clear();	
+					m_put_node(m_sentinal);
 				}
 
 				iterator begin() { return m_head; }
@@ -284,6 +317,77 @@ namespace fl
 				size_type size() const { return m_count; }
 
 				void clear()
+				{
+					node_ptr current = m_head;
+					while (current != m_sentinal)
+					{
+						node_ptr temp = current;
+						m_destroy_node(temp);
+						m_put_node(temp);
+						current = current->m_next;
+					}
+					current = nullptr;
+					m_head = m_sentinal;
+					m_tail = m_sentinal;
+				}
+
+				iterator insert(iterator pos, const T& value)
+				{
+					node_ptr new_node = m_acquire_node();
+					m_construct_node(new_node, value);
+					new_node->m_next = pos.m_node;
+					new_node->m_prev = pos.m_node->m_prev;
+					pos.m_node->m_prev->m_next = new_node;
+					pos.m_node->m_prev = new_node;
+
+					m_count++;
+					return iterator(new_node);
+				}
+				iterator insert(const_iterator pos, const T& value)
+				{
+					iterator temp(pos.m_node);
+					node_ptr new_node = m_acquire_node();
+					m_construct_node(new_node, value);
+					new_node->m_next = temp.m_node;
+					new_node->m_prev = temp.m_node->m_prev;
+					temp.m_node->m_prev->m_next = new_node;
+					temp.m_node->m_prev = new_node;
+					
+					m_count++;
+					return iterator(new_node);
+				}
+
+				iterator insert(const_iterator pos, T&& value);
+
+				iterator insert(iterator pos, size_type count, const T& value) 
+				{
+					if (count == 0)
+						return pos;
+
+					m_count += count;
+					node_ptr np;
+					for (int i = 0; i < (int) count; i++)
+					{
+						np = m_acquire_node();
+						m_construct_node(np, value);
+						np++;
+					}
+					np = np - count + 1;
+					for (int i = 0; i < (int) count-1; i++)
+					{
+						np[i].m_next = &np[i+1];
+						np[i+1].m_prev = &np[i];	
+					}
+					np[0].m_prev = pos.m_node->m_prev;
+					np[count-1].m_next = pos.m_node;
+					pos.m_node->m_prev->m_next = &np[0];
+					pos.m_node->m_prev = &np[count-1];
+
+					return iterator(&np[0]);
+				}
+
+				template <class InputIt>
+				iterator insert(iterator pos, InputIt first, InputIt last)
 				{
 				}
 
