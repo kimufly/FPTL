0bf3e7a46b467bf75745423aa9cfc62d11d8209f
Feng Pan <panfeng0007@gmail.com>
2015-04-27 09:55:00 -0400

    working on resize

--- Sources/Containers/List/List.hpp
+++ Sources/Containers/List/List.hpp
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <stdexcept>
 #include <type_traits>
+#include <algorithm>
 #include "../../Iterators/Iterator.hpp"
 
 namespace fl
@@ -13,21 +14,15 @@ namespace fl
 		struct Node_base;
 		template <class T> struct ListNode;
 		template <class T, class Allocator> class List;
+		template <class T, bool is_const = false> struct Iterator;
 
 		struct Node_base 
 		{
-			using pointer = Node_base*;
 			Node_base() : m_next(this), m_prev(this) {}
 
-			Node_base& operator=(Node_base& other) 
-			{
-				m_next = other.m_next;
-				m_prev = other.m_prev;
-				return *this;
-			}
 			virtual ~Node_base() {}
-			pointer m_next;
-			pointer m_prev;
+			Node_base* m_next;
+			Node_base* m_prev;
 		};
 
 		template <class T> struct ListNode : public Node_base
@@ -39,6 +34,75 @@ namespace fl
 			value_type m_data;
 		};
 
+		template <bool flag_is_const, class Const, class No_const> struct type_picker {};
+		template <class Const, class Non_const> struct type_picker<true, Const, Non_const>
+		{
+			using type = Const;
+		};
+
+		template <class Const, class Non_const> struct type_picker<false, Const, Non_const>
+		{
+			using type = Non_const;
+		};
+		template <class T, bool is_const> struct Iterator
+		{
+			using iterator_category = fl::iterators::BidirectionalIteratorTag;
+			using value_type = typename type_picker<is_const, const T, T>::type;
+			using difference_type = std::ptrdiff_t;
+			using pointer = typename type_picker<is_const, const T*, T*>::type;
+			using reference = typename type_picker<is_const, const T&, T&>::type;
+			using node_ptr = ListNode<T>*;
+
+			node_ptr m_node;	
+
+			Iterator() {}
+			Iterator(node_ptr p) : m_node(p) {}
+			Iterator(const Iterator& other) : m_node(other.m_node) {}
+			~Iterator() {}
+
+			reference operator*() const
+			{
+				return m_node->m_data;
+			}
+			pointer operator->() const
+			{
+				return &(operator*());
+			}
+			Iterator& operator++()
+			{
+				m_node = dynamic_cast<node_ptr>(m_node->m_next);
+				return *this;
+			}
+			Iterator operator++(int)
+			{
+				Iterator it(*this);
+				m_node = dynamic_cast<node_ptr>(m_node->m_next);
+				return it;
+			}
+
+			Iterator& operator--()
+			{
+				m_node = dynamic_cast<node_ptr>(m_node->m_prev);
+				return *this;
+			}
+			Iterator operator--(int)
+			{
+				Iterator it(*this);
+				m_node = dynamic_cast<node_ptr>(m_node->m_prev);
+				return it;
+			}
+
+			bool operator==(const Iterator& rhs)
+			{
+				return this->m_node == rhs.m_node;
+			}
+
+			bool operator!=(const Iterator& rhs)
+			{
+				return this->m_node != rhs.m_node;
+			}
+
+		};	
 
 		template < class T, class Allocator = std::allocator<T> >
 		class List
@@ -114,77 +178,8 @@ namespace fl
 					}
 				}
 
-
-				template <bool flag_is_const, class Const, class No_const> struct type_picker {};
-				template <class Const, class Non_const> struct type_picker<true, Const, Non_const>
-				{
-					using type = Const;
-				};
-
-				template <class Const, class Non_const> struct type_picker<false, Const, Non_const>
-				{
-					using type = Non_const;
-				};
-				template <class U, bool is_const = false> struct Iterator
-				{
-					using iterator_category = fl::iterators::BidirectionalIteratorTag;
-					using value_type = typename type_picker<is_const, const U, U>::type;
-					using difference_type = std::ptrdiff_t;
-					using pointer = typename type_picker<is_const, const U*, U*>::type;
-					using reference = typename type_picker<is_const, const U&, U&>::type;
-					using node_ptr = typename type_picker<is_const, const ListNode<U>*, ListNode<U>*>::type;
-
-					node_ptr m_node;	
-
-					Iterator() {}
-					Iterator(node_ptr p) : m_node(p) {}
-					Iterator(const Iterator& other) : m_node(other.m_node) {}
-					~Iterator() {}
-
-					reference operator*() const
-					{
-						return m_node->m_data;
-					}
-					pointer operator->() const
-					{
-						return &(operator*());
-					}
-					Iterator& operator++()
-					{
-						m_node = dynamic_cast<node_ptr>(m_node->m_next);
-						return *this;
-					}
-					Iterator operator++(int)
-					{
-						Iterator it(*this);
-						m_node = dynamic_cast<node_ptr>(m_node->m_next);
-						return it;
-					}
-
-					Iterator& operator--()
-					{
-						m_node = dynamic_cast<node_ptr>(m_node->m_prev);
-						return *this;
-					}
-					Iterator operator--(int)
-					{
-						Iterator it(*this);
-						m_node = dynamic_cast<node_ptr>(m_node->m_prev);
-						return it;
-					}
-
-					bool operator==(const Iterator& rhs)
-					{
-						return this->m_node == rhs.m_node;
-					}
-
-					bool operator!=(const Iterator& rhs)
-					{
-						return this->m_node != rhs.m_node;
-					}
-
-				};	
 			public:
+
 				using iterator = Iterator<T>;
 				using const_iterator = Iterator<T, true>;
 				using reverse_iterator = fl::iterators::ReverseIterator<iterator>;
@@ -196,6 +191,7 @@ namespace fl
 					clear();
 					m_initilize_dispatch(count, value, std::true_type());
 				}
+
 				template <class Integer>
 				void m_assign_dispatch(Integer count, Integer& value, std::true_type)
 				{
@@ -237,46 +233,42 @@ namespace fl
 				}
 				void m_insert(const_iterator pos, const T& value)
 				{
-					if (pos.m_node == m_head)
+					if (pos == cbegin())
 					{
 						push_front(value);
 					}
-					else if (pos == end())
+					else if (pos == cend())
 					{
 						push_back(value);
 					}
 					else
 					{
-						iterator temp(pos.m_node);
 						node_ptr new_node = m_acquire_node();
 						m_construct_node(new_node, value);
-						new_node->m_next = temp.m_node;
-						new_node->m_prev = temp.m_node->m_prev;
-						temp.m_node->m_prev->m_next = new_node;
-						temp.m_node->m_prev = new_node;
-						if (temp.m_node == m_tail)
+						new_node->m_next = pos.m_node;
+						new_node->m_prev = pos.m_node->m_prev;
+						pos.m_node->m_prev->m_next = new_node;
+						pos.m_node->m_prev = new_node;
+						if (pos.m_node == m_tail)
 						{
 							m_tail = new_node;
 							m_tail->m_next = m_sentinal;
 							m_sentinal->m_prev = m_tail;
 						}
 						m_count++;
-
 					}
 				}
 
-				void m_fill_insert(iterator pos, size_type count, const T& value)
+				void m_fill_insert(const_iterator pos, size_type count, const T& value)
 				{
-					iterator temp(pos.m_node);
 					for (int i = 0; i < (int) count; i++)
 					{
-						m_insert(temp, value);
-						--temp;
+						m_insert(pos, value);
 					}
 				}
 
 				template <class Integer>
-				void m_insert_dispatch(iterator pos, Integer count, Integer value, std::true_type)
+				void m_insert_dispatch(const_iterator pos, Integer count, Integer value, std::true_type)
 				{
 					m_fill_insert(pos, static_cast<size_type>(count), static_cast<value_type>(value));
 				}
@@ -284,12 +276,32 @@ namespace fl
 				template <class InputIt>
 				void m_insert_dispatch(const_iterator pos, InputIt first, InputIt last, std::false_type)
 				{
-					iterator temp(pos.m_node);
 
 					for (; first != last; ++first)
 					{
-						m_insert(temp, *first);
-						++temp;
+						m_insert(pos, *first);
+					}
+				}
+
+				void m_erase(const_iterator pos)
+				{
+					node_ptr tmp = pos.m_node;
+					if (pos.m_node->m_next == m_sentinal)
+					{
+						m_tail = dynamic_cast<node_ptr>(pos.m_node->m_prev);
+					}
+					
+					tmp->m_prev->m_next = pos.m_node->m_next;
+					tmp->m_next->m_prev = pos.m_node->m_prev;
+					m_destroy_node(pos.m_node);
+					m_put_node(pos.m_node);
+					m_count--;
+				}
+				void m_erase(const_iterator first, const_iterator last)
+				{
+					for (; first != last; ++first)
+					{
+						m_erase(first);
 					}
 				}
 
@@ -306,7 +318,9 @@ namespace fl
 
 				List(const List& other)
 				{
-					m_alloc = std::allocator_traits<alloc_type>::select_on_container_copy_constructor(other.get_allocator());
+					m_alloc = std::allocator_traits<alloc_type>::select_on_container_copy_construction(other.get_allocator());
+					m_sentinal = m_acquire_node();
+					m_construct_node(m_sentinal, 0);
 					m_initilize_dispatch(other.begin(), other.end(), std::false_type());
 					m_count = other.m_count;
 				}
@@ -363,7 +377,6 @@ namespace fl
 				List(std::initializer_list<T> init, const Allocator& alloc = Allocator())
 				{
 					m_alloc = alloc;
-					m_count = init.size();
 					m_sentinal = m_acquire_node();
 					m_construct_node(m_sentinal, 0);
 					m_head = m_sentinal;
@@ -383,12 +396,22 @@ namespace fl
 				List& operator=(const List& other)
 				{
 					assign(other.begin(), other.end());
+					return *this;
 				}
 				List& operator=(List&& other)
 				{
 					assign(other.begin(), other.end());
+					return *this;
 				}
-				List& operator=(std::initializer_list<T>& ilist);
+				List& operator=(std::initializer_list<T>& ilist)
+				{
+					this->clear();
+					for (auto it = ilist.begin(); it != ilist.end(); ++it)
+					{
+						push_back(*it);
+					}
+					return *this;
+				}
 
 				template <class InputIt>
 				void assign(InputIt first, InputIt last)
@@ -403,9 +426,9 @@ namespace fl
 				iterator begin() const { return m_head; }
 				const_iterator cbegin() const { return const_iterator(m_head); }
 
-				iterator end() { return iterator(dynamic_cast<node_ptr>(m_tail->m_next)); }
-				iterator end() const { return iterator(dynamic_cast<node_ptr>(m_tail->m_next)); }
-				const_iterator cend() const { return const_iterator(dynamic_cast<node_ptr>(m_tail->m_next)); }
+				iterator end() { return iterator(dynamic_cast<node_ptr>(m_sentinal)); }
+				iterator end() const { return iterator(dynamic_cast<node_ptr>(m_sentinal)); }
+				const_iterator cend() const { return const_iterator(dynamic_cast<node_ptr>(m_sentinal)); }
 
 				reverse_iterator rbegin() { return reverse_iterator(end()); }
 				reverse_iterator rbegin() const { return reverse_iterator(end()); }
@@ -422,27 +445,56 @@ namespace fl
 
 				bool empty()
 				{
-					return m_count == 0 && begin() == end();
+					return m_count == 0 && m_head == m_tail;
 				}
 
 				size_type size() const { return m_count; }
 
+				size_type max_size() const { return m_alloc.max_size(); }
+
+				void resize(size_type count)
+				{
+					if (count == 0)
+						this->clear();
+
+					if (m_count > count)
+					{
+						for (int i = 0; i < (int) m_count - (int) count; i++)
+						{
+							pop_back();
+						}
+						m_count = count;
+					}
+					if (m_count < count)
+					{
+						for (int i = 0; i < (int) count - (int) m_count; i++)
+							push_back(0);
+
+						m_count = count;
+					}
+				}
+				void resize(size_type count, const value_type& value);
+
 				void clear()
 				{
-					node_ptr current = m_head;
-					while (current != m_sentinal)
+					if (!empty())
 					{
-						node_ptr temp = current;
-						if (current)
+						node_ptr current = m_head;
+						while (current != m_sentinal)
 						{
-							m_destroy_node(temp);
-							m_put_node(temp);
+							node_ptr temp = current;
+							current = dynamic_cast<node_ptr>(current->m_next);
+							if (current)
+							{
+								m_destroy_node(temp);
+								m_put_node(temp);
+							}
 						}
-						current = dynamic_cast<node_ptr>(current->m_next);
+						current = nullptr;
+						m_count = 0;
+						m_head = m_sentinal;
+						m_tail = m_sentinal;
 					}
-					current = nullptr;
-					m_head = m_sentinal;
-					m_tail = m_sentinal;
 				}
 
 				iterator insert(iterator pos, const T& value)
@@ -452,30 +504,42 @@ namespace fl
 				}
 				iterator insert(const_iterator pos, const T& value)
 				{
-					iterator temp(pos.m_node);
-					m_insert(temp, value);
-					return --temp;
+					iterator ret(pos.m_node);
+					m_insert(ret, value);
+					return --ret;
 				}
 
 				iterator insert(const_iterator pos, T&& value)
 				{
-					iterator temp(pos.m_node);
-					m_insert(temp, std::move(value));
-					return --temp;
+					iterator ret(pos.m_node);
+					m_insert(ret, std::move(value));
+					return --ret;
 				}
 
 				template <class InputIt>
 				iterator insert(const_iterator pos, InputIt first, InputIt last)
 				{
 					using Integer = typename std::is_integral<InputIt>::type;
+					iterator ret(dynamic_cast<node_ptr>(pos.m_node->m_prev));
 					m_insert_dispatch(pos, first, last, Integer());
-					iterator temp(pos.m_node);
+					return ++ret;
+				}
+
+				iterator erase(const_iterator pos)
+				{
+					iterator ret(dynamic_cast<node_ptr>(pos.m_node->m_next));
+					m_erase(pos);
+					return ret;
+				}
+				iterator erase(const_iterator first, const_iterator last)
+				{
 					for (; first != last; ++first)
 					{
-						--temp;
+						m_erase(first);
 					}
-					return temp;
+					return iterator(last.m_node);
 				}
+
 
 				void push_front(const T& value)
 				{
@@ -500,6 +564,7 @@ namespace fl
 					m_count++;
 
 				}
+
 				void push_front(T&& value)
 				{
 					node_ptr new_node = m_acquire_node();
@@ -545,6 +610,7 @@ namespace fl
 					m_sentinal->m_next = m_head;
 					m_count++;
 				}
+
 				void push_back(T&& value)
 				{
 					node_ptr new_node = m_acquire_node();
@@ -570,7 +636,7 @@ namespace fl
 
 				void pop_front()
 				{
-					node_ptr new_head = m_head->m_next;
+					node_ptr new_head = dynamic_cast<node_ptr>(m_head->m_next);
 					new_head->m_prev = m_sentinal;
 					if (m_head)
 					{
@@ -581,7 +647,7 @@ namespace fl
 				}
 				void pop_back()
 				{
-					node_ptr new_tail = m_tail->m_prev;
+					node_ptr new_tail = dynamic_cast<node_ptr>(m_tail->m_prev);
 					new_tail->m_next = m_sentinal;
 					if (m_tail)
 					{
@@ -600,6 +666,140 @@ namespace fl
 				size_type m_count;
 				alloc_type m_alloc;
 		};
+
+		template <class T, class Alloc>
+		bool operator==(const List<T, Alloc>& lhs, const List<T, Alloc>& rhs)
+		{
+			if (lhs.size() == rhs.size())
+			{
+				auto it1 = lhs.cbegin();
+				auto it2 = rhs.cbegin();
+				for (; it1 != lhs.cend() && it2 != rhs.cend(); ++it1)
+				{
+					if (*it1 != *it2)
+						return false;
+					++it2;
+				}
+				return true;
+			}
+			return false;
+		}
+		template <class T, class Alloc>
+		bool operator!=(const List<T, Alloc>& lhs, const List<T, Alloc>& rhs)
+		{
+			if (lhs.size() == rhs.size())
+			{
+				auto it1 = lhs.cbegin();
+				auto it2 = rhs.cbegin();
+				for (; it1 != lhs.cend() && it2 != rhs.cend(); ++it1)
+				{
+					if (*it1 != *it2)
+						return true;
+					++it2;
+				}
+				return false;
+			}
+			return true;
+		}
+
+		template <class T, class Alloc>
+		bool operator<(const List<T, Alloc>& lhs, const List<T, Alloc>& rhs)
+		{
+			if (lhs.size() < rhs.size())
+			{
+				return true;
+			}
+			else if (lhs.size() > rhs.size())
+			{
+				return false;
+			}
+			else
+			{
+				auto it1 = lhs.cbegin();
+				auto it2 = rhs.cbegin();
+				for (; it1 != lhs.cend() && it2 != rhs.cend(); ++it1)
+				{
+					if (*it1 > *it2)
+						return false;
+					++it2;
+				}
+				return true;
+			}
+		}
+		template <class T, class Alloc>
+		bool operator<=(const List<T, Alloc>& lhs, const List<T, Alloc>& rhs)
+		{
+			if (lhs.size() < rhs.size())
+			{
+				return true;
+			}
+			else if (lhs.size() > rhs.size())
+			{
+				return false;
+			}
+			else
+			{
+				auto it1 = lhs.cbegin();
+				auto it2 = rhs.cbegin();
+				for (; it1 != lhs.cend() && it2 != rhs.cend(); ++it1)
+				{
+					if (*it1 > *it2)
+						return false;
+					++it2;
+				}
+				return true;
+			}
+
+		}
+		template <class T, class Alloc>
+		bool operator>(const List<T, Alloc>& lhs, const List<T, Alloc>& rhs)
+		{
+			if (lhs.size() > rhs.size())
+			{
+				return true;
+			}
+			else if (lhs.size() < rhs.size())
+			{
+				return false;
+			}
+			else
+			{
+				auto it1 = lhs.cbegin();
+				auto it2 = rhs.cbegin();
+				for (; it1 != lhs.cend() && it2 != rhs.cend(); ++it1)
+				{
+					if (*it1 < *it2)
+						return false;
+					++it2;
+				}
+				return true;
+			}
+		}
+
+		template <class T, class Alloc>
+		bool operator>=(const List<T, Alloc>& lhs, const List<T, Alloc>& rhs)
+		{
+			if (lhs.size() > rhs.size())
+			{
+				return true;
+			}
+			else if (lhs.size() < rhs.size())
+			{
+				return false;
+			}
+			else
+			{
+				auto it1 = lhs.cbegin();
+				auto it2 = rhs.cbegin();
+				for (; it1 != lhs.cend() && it2 != rhs.cend(); ++it1)
+				{
+					if (*it1 < *it2)
+						return false;
+					++it2;
+				}
+				return true;
+			}
+		}
 	}
 
 }
--- Tests/test_list.cpp
+++ Tests/test_list.cpp
@@ -4,68 +4,187 @@
 
 int main()
 {
+	// Testing constructors
 	std::cout << "Testing List constructors. \n";
+	std::cout << "Initialize lst1 with three 5's.\n";
 	ftd::List<int> lst1(3, 5);
+	std::cout << "Initialize lst2 with Initializer_list {1,2,3}\n";
 	ftd::List<int> lst2{1,2,3};
+	std::cout << "Initialize lst3 with copy of lst2\n";
 	ftd::List<int> lst3(lst2.begin(), lst2.end());
+	std::cout << "Initialize lst4 with copy constructor from lst3\n";
+	ftd::List<int> lst4(lst3);
 
+	// Testing Iterators
+
+	std::cout << "Testing iterators.\n";
 	
 	ftd::List<int>::iterator it = lst1.begin();
 	ftd::List<int>::const_iterator cit = lst1.cbegin();
 	ftd::List<int>::reverse_iterator rit = lst1.rbegin();
 	ftd::List<int>::const_reverse_iterator rcit = lst1.rcbegin();
 
-	std::cout << "Testing iterator for lst1:\n";
+	std::cout << "Iterator: \n";
 	for (; it != lst1.end(); ++it)
-	{
-		std::cout << *it << std::endl;
-	}
+		std::cout << *it << " ";
+	std::cout << std::endl;
 
-	std::cout << "Testing const iterator for lst1:\n";
+	std::cout << "Const_iterator: \n";
 	for (; cit != lst1.cend(); ++cit)
-	{
-		std::cout << *cit << std::endl;
-	}
+		std::cout << *cit << " ";
+	std::cout << std::endl;
 
-	std::cout << "Testing reverse iterator for lst1: \n";
+	std::cout << "Reverse_iterator: \n";
 	for (; rit != lst1.rend(); ++rit)
-	{
-		std::cout << *rit << std::endl;
-	}
-	std::cout << "Testing const reverse iterator for lst1: \n";
+		std::cout << *rit << " ";
+	std::cout << std::endl;
+
+	std::cout << "Const_reverse_iterator: \n";
 	for (; rcit != lst1.rcend(); ++rcit)
-	{
-		std::cout << *rcit << std::endl;
-	}
+		std::cout << *rcit << " ";
+	std::cout << std::endl;
 
+	std::cout << "Testing insertion: \n";
+	std::cout << "Single insert: \n";
+	auto ret = lst2.insert(lst2.end(), 4);
+	std::cout << "Value inserted: " << *ret << std::endl;
+	ret = lst2.insert(lst2.end(), 5);
+	std::cout << "Value inserted: " << *ret << std::endl;
 	it = lst2.begin();
-	cit = lst2.cbegin();
-	rit = lst2.rbegin();
-	rcit = lst2.rcbegin();
-	
-	std::cout << "Testing iterator for lst2:\n";
 	for (; it != lst2.end(); ++it)
-	{
-		std::cout << *it << std::endl;
-	}
+		std::cout << *it << " ";
+	std::cout << std::endl;
 
-	std::cout << "Testing const_iterator for lst2:\n";
-	for (; cit != lst2.cend(); ++cit)
-	{
-		std::cout << *cit << std::endl;
-	}
+	std::cout << "Copy insert: \n";
+	ret = lst2.insert(lst2.cend(), 5, 6);
+	it = lst2.begin();
+	for (; it != lst2.end(); ++it)
+		std::cout << *it << " ";
+	std::cout << std::endl;
 
-	std::cout << "Testing reverse_iterator for lst2:\n";
-	for (; rit != lst2.rend(); ++rit)
-	{
-		std::cout << *rit << std::endl;
-	}
+	std::cout << "Testing assignment:\n";
+	std::cout << "lst4 = lst2\n";
+	lst4 = lst2;
+	it = lst4.begin();
+	for (; it != lst4.end(); ++it)
+		std::cout << *it << " ";
+	std::cout << std::endl;
 
-	std::cout << "Testing const_reverse_iterator for lst2:\n";
-	for (; rcit != lst2.rcend(); ++rcit)
+	std::cout << "Testing erase functions: \n";
+	cit = lst4.cend();
+	--cit;
+	ret = lst4.erase(cit);
+	std::cout << "Last element deleted.\n";
+
+	for (it = lst4.begin(); it != lst4.end(); ++it)
+		std::cout << *it << " ";
+	std::cout << std::endl << std::endl;
+
+	auto itbegin = lst4.cbegin();
+	auto itend = lst4.cend();
+
+	++itbegin;
+	--itend;
+
+	ret = lst4.erase(itbegin, itend);
+
+	for (it = lst4.begin(); it != lst4.end(); ++it)
+		std::cout << *it << " ";
+	std::cout << std::endl;
+
+	std::cout << "Creating lst5 with {1,2,3,4,5,6,7,8,9,0}.\n";
+	ftd::List<int> lst5{1,2,3,4,5,6,7,8,9,0};
+	for (auto &e : lst5)
 	{
-		std::cout << *rcit << std::endl;
+		std::cout << e << " ";
 	}
+	std::cout << std::endl;
+	std::cout << "Testing pop front and pop back:\n";
+	lst5.pop_front();
+	lst5.pop_back();
+	for (auto &e : lst5)
+		std::cout << e << " ";
+	std::cout << std::endl;
+
+	std::cout << "Testing clear function\n";
+	lst5.clear();
+	if (lst5.empty())
+		std::cout << "List cleared.\n";
+	for (auto &e : lst5)
+		std::cout << e << " ";
+	std::cout <<std::endl;
+
+	std::cout << "Testing push back and push front: \n";
+	std::cout << "Pushing 5 at the back\n";
+	lst5.push_back(5);
+	std::cout << "Pushing 6 at the back\n";
+	lst5.push_back(6);
+	std::cout << "Pushing 1 at the front\n";
+	lst5.push_front(1);
+	std::cout << "Pushing 12 at the front\n";
+	lst5.push_front(12);
+
+	for (auto &e : lst5)
+		std::cout << e << " ";
+	std::cout <<std::endl;
 	
+	std::cout << "Testing size: \n";
+	ftd::List<int> lst6;
+	std::cout << "Inserting 100 elements of 2.\n";
+	lst6.insert(lst6.cbegin(), 100, 2);
+	std::cout << "lst6 has " << lst6.size() << " elements of value " << *lst6.begin() << std::endl;
+
+	std::cout << "Testing resize:\n";
+	std::cout << "lst6 after resize: \n";
+	lst6.resize(5);
+	for (auto &e: lst6)
+		std::cout << e << " ";
+	std::cout << std::endl;
+
+	lst6.resize(10);
+	for (auto &e: lst6)
+		std::cout << e << " ";
+	std::cout << std::endl;
+	std::cout << lst6.size() << std::endl;
+
+	std::cout << "Testing operators\n";
+	ftd::List<char> lst7{'a', 'b', 'c', 'd'};
+	ftd::List<char> lst8{'z', 'y', 'r', 'x'};
+
+	ftd::List<char> lst9{'a', 'b', 'c', 'd'};
+	ftd::List<char> lst10{'a', 'b', 'f', 'd'};
+
+	bool less1 = lst7 < lst8;
+	bool less2 = lst9 < lst10;
+	bool equal = lst7 == lst9;
+	bool greater1 = lst8 > lst7;
+	bool greater2 = lst10 > lst9;
+	bool lessequal = lst7 <= lst9;
+	bool greaterequal = lst7 >= lst9;
+
+	std::cout << "lst7: ";
+	for (auto &e : lst7)
+		std::cout << e << " ";
+	std::cout << std::endl;
+	std::cout << "lst8: ";
+	for (auto &e : lst8)
+		std::cout << e << " ";
+	std::cout << std::endl;
+	std::cout << "lst9: ";
+	for (auto &e : lst9)
+		std::cout << e << " ";
+	std::cout << std::endl;
+	std::cout << "lst10: ";
+	for (auto &e : lst10)
+		std::cout << e << " ";
+	std::cout << std::endl;
+
+	std::cout << "lst7 < lst8? " << std::boolalpha << less1 << std::endl;
+	std::cout << "lst8 > lst7? " << std::boolalpha << greater1 << std::endl;
+	std::cout << "lst7 == lst9? " << std::boolalpha << equal << std::endl;
+	std::cout << "lst7 <= lst9? " << std::boolalpha << lessequal << std::endl;
+	std::cout << "lst7 >= lst9? " << std::boolalpha << greaterequal << std::endl;
+	std::cout << "lst9 < lst10? " << std::boolalpha << less2 << std::endl;
+	std::cout << "lst10 > lst9? " << std::boolalpha << greater2 << std::endl;
 	return 0;
 }
